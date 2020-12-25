69bcdcfe85f4149aae9fb2f501a076034ef35337
SigveMartin <sigvemar@gmail.com>
2020-12-25 10:50:31 +0100

    made changes to sense function after project review

--- robot_class.py
+++ robot_class.py
@@ -94,7 +94,7 @@ class robot:
             dx = lmrk[0] - self.x + self.rand() * self.measurement_noise
             dy = lmrk[1] - self.y + self.rand() * self.measurement_noise
             
-            if (dx < self.measurement_range) and (dy < self.measurement_range):
+            if (self.measurement_range == -1) or (abs(dx) <= self.measurement_range) and (abs(dy) <= self.measurement_range):
                 measurements.append([index,dx,dy])
             
         ## TODO: return the final, complete list of measurements
